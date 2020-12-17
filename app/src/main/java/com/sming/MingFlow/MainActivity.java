0cde66287b8f8808233964b53aa76dda701c0a67
zhan0313 <zhan0313@163.com>
2020-12-17 20:41:41 +0800

    首次提交

---
+++ app/src/main/java/com/sming/MingFlow/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sming.MingFlow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
